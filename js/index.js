98b5861f3604ad9e6883ccfab89a5392f06c90dd
eupereirathiago <thiagopereira409@gmail.com>
2024-03-14 12:06:19 -0300

    add

--- js/index.js
+++ js/index.js
@@ -15,8 +15,16 @@ async function getProducts(){
     const request = await fetch('https://fakestoreapi.com/products');
     const response = await request.json();
     if(response.length > 0){
+        produtos.innerHTML = '';
         response.map(produto => {
-            produtos.innerHTML += `<li>${produto.title}</li>`;
+            produtos.innerHTML += `
+            <li>
+            <img src="${produto.image}">
+            <h2>${produto.title}</h2>
+            <h3>${produto.category}</h3>
+            <p>${produto.description}</p>
+            <h4>R$${produto.price} <span><box-icon type='solid' name='star'></box-icon>${produto.rating.rate}</span></h4>
+        </li>`;
         })
         return;
     }
